3f2e7be6231db528561831d8bef4b03658fba80e
Gabriel <gabrielcsg19@hotmail.com>
2023-06-05 15:26:54 -0300

    update url of curriculum

--- src/components/Home/HomeInfo/index.tsx
+++ src/components/Home/HomeInfo/index.tsx
@@ -24,7 +24,7 @@ export function HomeInfo() {
       </Box>
 
       <Link
-        href="https://drive.google.com/file/d/15zbhH3M_Ye5cZPxkVETBhTGj3d1zNhD6/view?usp=share_link"
+        href="https://drive.google.com/file/d/1E6ymEv9HcVcQx2pngHt3XiFivbVryERB/view?usp=sharing"
         target="_blank"
         _hover={{ textDecoration: 'none' }}
         alignSelf={{ base: 'center', md: 'flex-start' }}
